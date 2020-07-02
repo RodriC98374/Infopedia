6500e120ae921df5f3108765e51f637700d3e62f
Rodrigo C <rodricesp.3.0@gmail.com>
2020-07-02 00:56:00 -0400

    Cambios en el menu de navegacion

---
+++ script/manu-resp.js
@@ -0,0 +1,9 @@
+JQuery('document').ready(function($){
+
+    var menuBtn = $('')
+
+    /* $('nav ul li.btn span').click(function(){
+        $('nav ul div.items').togglerClass("show");
+        $('nav ul li.btn span').togglerClass("show");
+    })*/
+});
